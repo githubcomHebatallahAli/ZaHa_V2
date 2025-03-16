cbe3d3003aaeac05497d51277aba8c45a3ffe3a5
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-03-17 00:58:27 +0200

    update

--- app/Http/Controllers/Admin/PortfolioController.php
+++ app/Http/Controllers/Admin/PortfolioController.php
@@ -100,29 +100,115 @@ class PortfolioController extends Controller
 
 
 
-    public function update(PortfolioRequest $request, string $id)
-    {
-        $this->authorize('manage_users');
-       $Portfolio =Portfolio::findOrFail($id);
+//     public function update(PortfolioRequest $request, string $id)
+//     {
+//         $this->authorize('manage_users');
+//        $Portfolio =Portfolio::findOrFail($id);
 
-       if (!$Portfolio) {
-        return response()->json([
-            'message' => "Portfolio not found."
-        ], 404);
+//        if (!$Portfolio) {
+//         return response()->json([
+//             'message' => "Portfolio not found."
+//         ], 404);
+//     }
+//        $Portfolio->update([
+//             "name" => $request->name,
+//             "description" => $request->description,
+//             "programLang" => $request-> programLang,
+//             "startDate" => $request->startDate,
+//             "endDate" => $request->endDate,
+//             "url" => $request->url,
+//             "projectType" => $request-> projectType,
+//             "videoUrl" => $request->videoUrl,
+//             "status" => $request-> status,
+//         ]);
+
+//         if ($request->hasFile('mainImage')) {
+//             if ($Portfolio->mainImage) {
+//                 Storage::disk('public')->delete($Portfolio->mainImage);
+//             }
+//             $mainImagePath = $request->file('mainImage')->store('Portfolio', 'public');
+//             $Portfolio->mainImage = $mainImagePath;
+//         }
+
+//         if ($request->hasFile('images')) {
+//             if ($Portfolio->images) {
+//                 foreach ($Portfolio->images as $oldImage) {
+//                     Storage::disk('public')->delete($oldImage);
+//                 }
+//             }
+
+//             $imagesPaths = [];
+
+//             foreach ($request->file('images') as $image) {
+//                 $path = $image->store('Portfolio', 'public');
+
+//                 $imagesPaths[] = $path;
+//             }
+
+//             $Portfolio->images = $imagesPaths;
+//         }
+
+//         // if ($request->hasFile('video')) {
+//         //     if ($Portfolio->video) {
+//         //         Storage::disk('public')->delete($Portfolio->video);
+//         //     }
+//         //     $videoPath = $request->file('video')->store('Portfolio', 'public');
+//         //     $Portfolio->video = $videoPath;
+//         // }
+
+//        $Portfolio->save();
+//        return response()->json([
+//         'data' =>new PortfolioResource($Portfolio),
+//         'message' => " Update Portfolio By Id Successfully."
+//     ]);
+
+//   }
+
+     public function update(PortfolioRequest $request, string $id)
+     {
+         $this->authorize('manage_users');
+        $Portfolio =Portfolio::findOrFail($id);
+
+        if (!$Portfolio) {
+         return response()->json([
+             'message' => "Portfolio not found."
+         ], 404);
+     }
+
+     if ($request->filled('name')) {
+        $Portfolio->name = $request->name;
     }
-       $Portfolio->update([
-            "name" => $request->name,
-            "description" => $request->description,
-            "programLang" => $request-> programLang,
-            "startDate" => $request->startDate,
-            "endDate" => $request->endDate,
-            "url" => $request->url,
-            "projectType" => $request-> projectType,
-            "videoUrl" => $request->videoUrl,
-            "status" => $request-> status,
-        ]);
+     if ($request->filled('description')) {
+        $Portfolio->description = $request->description;
+    }
+     if ($request->filled('programLang')) {
+        $Portfolio->programLang = $request->programLang;
+    }
 
-        if ($request->hasFile('mainImage')) {
+
+
+     if ($request->filled('url')) {
+        $Portfolio->url = $request->url;
+    }
+
+    if ($request->filled('startDate')) {
+        $Portfolio->startDate = $request->startDate;
+    }
+
+     if ($request->filled('endDate')) {
+        $Portfolio->endDate = $request->endDate;
+    }
+     if ($request->filled('projectType')) {
+        $Portfolio->projectType = $request->projectType;
+    }
+     if ($request->filled('videoUrl')) {
+        $Portfolio->videoUrl = $request->videoUrl;
+    }
+     if ($request->filled('status')) {
+        $Portfolio->status = $request->status;
+    }
+
+            if ($request->hasFile('mainImage')) {
             if ($Portfolio->mainImage) {
                 Storage::disk('public')->delete($Portfolio->mainImage);
             }
@@ -148,21 +234,14 @@ class PortfolioController extends Controller
             $Portfolio->images = $imagesPaths;
         }
 
-        // if ($request->hasFile('video')) {
-        //     if ($Portfolio->video) {
-        //         Storage::disk('public')->delete($Portfolio->video);
-        //     }
-        //     $videoPath = $request->file('video')->store('Portfolio', 'public');
-        //     $Portfolio->video = $videoPath;
-        // }
+             $Portfolio->save();
+        return response()->json([
+         'data' =>new PortfolioResource($Portfolio),
+         'message' => " Update Portfolio By Id Successfully."
+     ]);
 
-       $Portfolio->save();
-       return response()->json([
-        'data' =>new PortfolioResource($Portfolio),
-        'message' => " Update Portfolio By Id Successfully."
-    ]);
+   }
 
-  }
 
   public function destroy(string $id)
   {
