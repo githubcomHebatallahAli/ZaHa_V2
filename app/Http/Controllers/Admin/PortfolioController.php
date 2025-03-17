321386af4747eca268a5e284a1f69013f80574b1
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-03-17 04:27:47 +0200

    update

--- app/Http/Controllers/Admin/PortfolioController.php
+++ app/Http/Controllers/Admin/PortfolioController.php
@@ -223,15 +223,55 @@ class PortfolioController extends Controller
     $slug = $this->generateSlug($Portfolio->name, $Portfolio->id);
     $Portfolio->slug = $slug;
 
-            if ($request->hasFile('mainImage')) {
+        //     if ($request->hasFile('mainImage')) {
+        //     if ($Portfolio->mainImage) {
+        //         Storage::disk('public')->delete($Portfolio->mainImage);
+        //     }
+        //     $mainImagePath = $request->file('mainImage')->store('Portfolio', 'public');
+        //     $Portfolio->mainImage = $mainImagePath;
+        // }
+
+        // if ($request->hasFile('images')) {
+        //     if ($Portfolio->images) {
+        //         foreach ($Portfolio->images as $oldImage) {
+        //             Storage::disk('public')->delete($oldImage);
+        //         }
+        //     }
+
+        //     $imagesPaths = [];
+
+        //     foreach ($request->file('images') as $image) {
+        //         $path = $image->store('Portfolio', 'public');
+
+        //         $imagesPaths[] = $path;
+        //     }
+
+        //     $Portfolio->images = $imagesPaths;
+        // }
+
+        //         // if ($request->hasFile('video')) {
+//         //     if ($Portfolio->video) {
+//         //         Storage::disk('public')->delete($Portfolio->video);
+//         //     }
+//         //     $videoPath = $request->file('video')->store('Portfolio', 'public');
+//         //     $Portfolio->video = $videoPath;
+//         // }
+
+
+        //      $Portfolio->save();
+
+        if ($request->hasFile('mainImage')) {
             if ($Portfolio->mainImage) {
                 Storage::disk('public')->delete($Portfolio->mainImage);
             }
             $mainImagePath = $request->file('mainImage')->store('Portfolio', 'public');
             $Portfolio->mainImage = $mainImagePath;
+        } else {
+            $mainImagePath = $Portfolio->mainImage;
         }
 
         if ($request->hasFile('images')) {
+
             if ($Portfolio->images) {
                 foreach ($Portfolio->images as $oldImage) {
                     Storage::disk('public')->delete($oldImage);
@@ -239,17 +279,17 @@ class PortfolioController extends Controller
             }
 
             $imagesPaths = [];
-
             foreach ($request->file('images') as $image) {
                 $path = $image->store('Portfolio', 'public');
-
                 $imagesPaths[] = $path;
             }
 
             $Portfolio->images = $imagesPaths;
+        } else {
+            $imagesPaths = $Portfolio->images;
         }
 
-             $Portfolio->save();
+         $Portfolio->save();
         return response()->json([
          'data' =>new PortfolioResource($Portfolio),
          'message' => " Update Portfolio By Id Successfully."
