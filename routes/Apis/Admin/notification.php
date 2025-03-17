eda08bef5c5c97540d2e34814d922d7db42f1305
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-03-17 21:18:54 +0200

    update

--- routes/Apis/Admin/notification.php
+++ routes/Apis/Admin/notification.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Admin\NotificationController;
 
 Route::controller(NotificationController::class)->prefix('/admin')->middleware('admin')->group(
     function () {
-        Route::get('showAll/notifications',  'showAll');
+    Route::get('showAll/notifications',  'showAll');
     Route::get('unread/notifications', 'unread');
     Route::post('markReadAll/notifications', 'markReadAll');
     Route::delete('deleteAll/notifications', 'deleteAll');
