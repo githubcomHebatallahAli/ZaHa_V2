3fc18b32c68377e52f198756aa2e25e1992fab65
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-07-25 02:07:32 +0300

    up

--- routes/Apis/Admin/order.php
+++ routes/Apis/Admin/order.php
@@ -8,11 +8,17 @@ use App\Http\Controllers\Admin\OrderController;
 Route::controller(OrderController::class)->prefix('/admin')->middleware('admin')->group(
     function () {
 
-   Route::get('/showAll/order','showAll');
-   Route::get('/edit/order/{id}','edit');
-   Route::delete('/delete/order/{id}', 'destroy');
-   Route::get('/showDeleted/order', 'showDeleted');
+Route::get('/showAll/order','showAll');
+Route::get('/edit/order/{id}','edit');
+Route::delete('/delete/order/{id}', 'destroy');
+Route::get('/showDeleted/order', 'showDeleted');
 Route::get('/restore/order/{id}','restore');
 Route::delete('/forceDelete/order/{id}','forceDelete');
+Route::patch('/pending/order/{id}', 'pending');
+Route::patch('/replied/order/{id}', 'replied');
+
+
+
+
 
    });
